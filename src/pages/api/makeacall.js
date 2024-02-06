ec050708b11850e6f73a3598843691ed3c4d08f6
kirtirajsinh <kirtiraj3738@gmail.com>
2024-02-06 15:12:26 +0530

    Refactor makeacall.js to use async/await for Twilio API call

--- src/pages/api/makeacall.js
+++ src/pages/api/makeacall.js
@@ -2,17 +2,16 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const to = req.body;
 
   try {
-    const value = client.calls
-      .create({
-        url: "http://demo.twilio.com/docs/voice.xml",
-        to: to,
-        from: process.env.TWILIO_PHONE_NUMBER,
-      })
-      .then((call) => console.log(call.sid));
+    const value = await client.calls.create({
+      url: "http://demo.twilio.com/docs/voice.xml",
+      to: to,
+      from: process.env.TWILIO_PHONE_NUMBER,
+    });
+    console.log(value);
 
     res.status(200).json(value);
   } catch (error) {
